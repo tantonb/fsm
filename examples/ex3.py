0a70e22031654a5e0b2b8b4360b83aa04bffafc1
tyler <tyler@fifoengineering.com>
2021-05-06 13:58:15 -0700

    new example, useful for REPL

---
+++ examples/ex3.py
@@ -0,0 +1,28 @@
+doc = """
+# a book
+start_state: closed
+
+states: [closed, page1, page2]
+
+transitions:
+
+    - action:       open
+      from_state:   closed
+      to_state:     page1
+
+    - action:       forward
+      from_state:   page1
+      to_state:     page2
+
+    - action:       back
+      from_state:   page2
+      to_state:     page1
+
+    - action:       close
+      from_state:   page1
+      to_state:     closed
+
+    - action:       close
+      from_state:   page2
+      to_state:     closed
+"""
